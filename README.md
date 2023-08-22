fa06ece820413c93663e37cceac90e4706bdc05e
IcyBow <contact@icybow.com>
2023-08-22 14:48:05 +0000

    :construction: DB init + html + css + DB reading

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Simple webapp to manage the payment of your family subscription.
 - \[Once it works well\] Dockerize project
 ## Developement
 I will work on this in my spare time as it is not something I need urgently. If you have any suggestion or question, you can [reach me](mailto:contact@icybow.com).
+This project will also be used for me to work on my *native JS* skills, so there will be no or very few external libraries. I am also not a big designer and it is not a very serious project that I am not even sure I will use productively (an Excel sheet does exactly the same(I already have it with all history)), but if you feel like improving the design (or anything else), feel free to do so.
--- initializeProject.php
+++ initializeProject.php
@@ -1,5 +1,9 @@
 <?php 
-
+if (isset($_GET['demo_data'])) {
+    $demo = true;
+} else {
+    $demo = false;
+}
 $db = new SQLite3('db/fsm.db');
 // Tables
 // -- Users table
@@ -16,7 +20,7 @@ email_recap_frequency INTEGER, -- 0:Daily, 1:Weekly, 2:Monthly
 admin INTEGER DEFAULT 0,
 active INTEGER DEFAULT 1)");
 // ---- Test Data
-// TODO: Create test if parameter create test data
+if ($demo) {
 $db->exec("INSERT INTO users(name, email, phone, uuid, pw, email_recap, email_recap_frequency, admin) 
 VALUES('Developer A', 'cntac@dfs.com', '00312545542', '123b04f6-0243-4ed8-bdeb-2095c14c578e', 'fafwae', 1, 2, 1)");
 $db->exec("INSERT INTO users(name, email, phone, uuid, pw, email_recap, email_recap_frequency, admin) 
@@ -25,6 +29,7 @@ $db->exec("INSERT INTO users(name, email, phone, uuid, pw, email_recap, email_re
 VALUES('User 1', 'sadfc@dffsa.de', '0031112312', 'f530a7ef-acbe-4a07-8654-24c7efb8c9ca', 'fajnl', 1, 3)");
 $db->exec("INSERT INTO users(name, email, phone, uuid, pw, email_recap, email_recap_frequency) 
 VALUES('Developer A', 'cntac@dfs.be', '05645441', 'df54aff8-5b98-43e1-a603-7bbd48d5d508', 'kgggte', 1, 1)");
+}
 
 // -- Subscriptions table
 $db->exec("DROP TABLE IF EXISTS subscriptions");
@@ -39,13 +44,14 @@ auto_create_day INT DEFAULT 0, -- Day on which monthly payments are created
 auto_create_month INT DEFAULT 0, -- Month on which yearly payments are created (+ day)
 active INT DEFAULT 1)");
 // ---- Test Data
-// TODO: Create test if parameter create test data
+if ($demo) {
 $db->exec("INSERT INTO subscriptions(name, actual_price, actual_payers, payment_frequency, auto_create, auto_create_day) 
 VALUES('Netflix', 15.99, 5, 3, 1, 1)");
 $db->exec("INSERT INTO subscriptions(name, actual_price, actual_payers, payment_frequency, auto_create, auto_create_day) 
 VALUES('Spotify', 17.99, 6, 3, 1, 1)");
 $db->exec("INSERT INTO subscriptions(name, actual_price, actual_payers, payment_frequency, auto_create, auto_create_day, auto_create_month) 
 VALUES('Disney Plus', 74.5, 5, 4, 1, 15, 9)");
+}
 
 // -- Payments table
 $db->exec("DROP TABLE IF EXISTS subscriptionPayments");
@@ -53,17 +59,18 @@ $db->exec("CREATE TABLE subscriptionPayments(
 id INTEGER PRIMARY KEY, 
 subscription INTEGER, -- Foreign id to subscriptions.id
 date INTEGER, 
-payers INTEGER DEFAULT 1,
+payers INTEGER DEFAULT 2,
 comments TEXT,
 FOREIGN KEY(subscription) REFERENCES subscriptions(id))");
 // ---- Test Data
-// TODO: Create test if parameter create test data
+if ($demo) {
 $db->exec("INSERT INTO subscriptionPayments(subscription, date, payers, comments) 
-VALUES(1, 1680310800, 5, 'all good')");
+VALUES(1, 1680310800, 3, 'all good')");
 $db->exec("INSERT INTO subscriptionPayments(subscription, date, payers, comments) 
-VALUES(1, 1682902800, 5, 'not so great')");
+VALUES(1, 1682902800, 3, 'not so great')");
 $db->exec("INSERT INTO subscriptionPayments(subscription, date, payers, comments) 
-VALUES(1, 1685581200, 5, 'ok')");
+VALUES(1, 1685581200, 3, 'ok')");
+}
 
 // Make check if data is available ? DO NOTHING unless you have a certain php parameter : Initialize DB 
 
